be286a7378faf8135628a83816921fe4e948f6cc
Danny Fullerton <northox@mantor.org>
2014-05-17 16:08:42 -0400

    Cleanup using struct

--- ok-rht03d/rht03.c
+++ ok-rht03d/rht03.c
@@ -15,44 +15,99 @@
 #include <wiringPi.h>
 #include <maxdetect.h>
 
-#define	RHT03_PIN 7
+#define	GPIO_PIN 7
 #define SAMPLE_SIZE 4
 #define SLEEP 20000
 #define SLEEP_NODATA 5000
 #define OFFCHART 10
+ 
+char * temp_path = "/var/cache/overkill/rht03/t";
+char * humi_path = "/var/cache/overkill/rht03/h";
+ 
+int si; // sample index
+ 
+typedef struct sensor {
+  int old;
+  int new;
+  int sample[SAMPLE_SIZE];
+  int avg;
+  char * path;
+} Sensor;
 
-const char * tfile = "/var/cache/overkill/rht03/t";
-const char * hfile = "/var/cache/overkill/rht03/h";
+typedef struct rht03 {
+  Sensor temp;
+  Sensor humi;
+} Rht03;
 
-void writebuf(const char *file, int *buf)
+// TODO reuse file descriptors 
+void writebuf(char *file, int *buf)
 {
   FILE *f = fopen(file, "w");
-  if (f == NULL)
-  {
+  if (f == NULL) {
     fprintf(stderr, "Can't open output file %s!\n", file);
     exit(1);
   }
-
+ 
   fprintf(f, "%1.1f\n", *buf / 10.0);
   fclose(f);
 }
 
-// move index across the array
-void step (int *pi)
+void step_si() 
 {
-  if (*pi == SAMPLE_SIZE - 1)
-    *pi = 0;
+  if (si == SAMPLE_SIZE - 1)
+    si = 0;
   else
-    *pi += 1;
+    si += 1;
+}
+ 
+void set(Sensor * s)
+{
+  if (s->new > s->old + OFFCHART || s->new < s->old - OFFCHART)
+    s->new = s->avg;
+     
+  s->sample[si] = s->old = s->new;
+  writebuf(s->path , &s->new);
 }
 
-int main (int argc, char* argv[])
+void set_avg(Sensor * s)
+{
+  int i;
+  int total;
+
+  total = 0;
+  for (i=0; i < SAMPLE_SIZE; i++)
+    total += s->sample[i];
+  
+  s->avg = total / SAMPLE_SIZE;
+}
+ 
+void init(Rht03 * r)
+{
+  r->temp.path = &temp_path;
+  r->humi.path = &humi_path;
+  
+  for(;;) {
+    if (readRHT03 (GPIO_PIN, &r->temp.new, &r->humi.new))
+      break;
+    delay(SLEEP_NODATA);
+  }
+
+  r->temp.avg = r->temp.old = r->temp.new;
+  r->humi.avg = r->humi.old = r->humi.new;
+  
+  int i;
+  for (i=0; i<SAMPLE_SIZE; i++) {
+    r->temp.sample[i] = r->temp.new;
+    r->humi.sample[i] = r->humi.new;
+  }
+}
+ 
+int main (void)
 {
   pid_t process_id = 0;
   pid_t sid = 0;
   process_id = fork();
-  if (process_id < 0)
-  {
+  if (process_id < 0) {
     fprintf(stderr, "Error: fork()\n");
     exit(1);
   } 
@@ -67,73 +122,35 @@ int main (int argc, char* argv[])
   close(STDIN_FILENO);
   close(STDOUT_FILENO); 
   close(STDERR_FILENO);  
-
-  // TODO use a struct and get rid of duplication
-  int temp, humi, ntemp, nhumi, ttemp, thumi, atemp, ahumi;
-  int index, i;
-  bool first;
-  int temps[SAMPLE_SIZE];
-  int humis[SAMPLE_SIZE];
-  int *pindex = &index;
-
-  temp = humi = ntemp = nhumi = ttemp = thumi = atemp = ahumi = 0;
-  i = index = 0;
-  first = true;
+ 
+  Rht03 r;
   
   wiringPiSetup();
   piHiPri(55);
+ 
+  si = 0; 
+  init(&r);
 
-  for(;;)
+  for(;;) 
   {
-    // no new data
-    if (!readRHT03 (RHT03_PIN, &ntemp, &nhumi)) {
+    if (!readRHT03 (GPIO_PIN, &r.temp.new, &r.humi.new)) {
       delay(SLEEP_NODATA);
       continue;
     }
-    
-    // initialize samples
-    if (first == true)
-    {
-      for (i=0; i<SAMPLE_SIZE; i++)
-      {
-        temps[i] = ntemp;
-        humis[i] = nhumi;
-      }
-      first = false;
-    } 
+ 
+    set_avg(&r.temp);
+    set_avg(&r.humi);
 
-    // Compute average
-    ttemp = thumi = 0;
-    for (i=0; i<SAMPLE_SIZE; i++)
-    {
-      ttemp += temps[i];
-      thumi += humis[i];
-    }
-    atemp = ttemp / SAMPLE_SIZE;
-    ahumi = thumi / SAMPLE_SIZE;
-
-    // some new data but not equal to lastest
-    if (temp != ntemp)
-    {
-      // use average if offchart
-      if (ntemp > temp + OFFCHART || ntemp < temp - OFFCHART)
-        ntemp = atemp;
-      
-      temps[index] = temp = ntemp;
-      writebuf(tfile, &temp);
-    }
-    if (humi != nhumi)
-    {
-      if (nhumi > humi + OFFCHART || nhumi < humi - OFFCHART) 
-        nhumi = ahumi;
-      
-      humis[index] = humi = nhumi;
-      writebuf(hfile , &humi);
-    }
+    if (r.temp.old != r.temp.new)
+      set(&r.temp);
     
-    step(pindex);
+    if (r.humi.old != r.humi.new)
+      set(&r.humi);
+    
+    
+    step_si();
     delay(SLEEP);
   }
-
+  
   return(0);
 }
